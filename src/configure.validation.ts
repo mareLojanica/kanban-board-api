8803f0f0592c1877f1757c965639ff7477d908af
Marko Lojanica <marko.lojanica1991@gmail.com>
2024-11-16 01:50:19 +0100

    Added env validation and cors

--- src/configure.validation.ts
+++ src/configure.validation.ts
@@ -3,4 +3,5 @@ import * as Joi from '@hapi/joi';
 export const envValidationSchema = Joi.object({
   MONGO_URI: Joi.string().required(),
   PORT: Joi.number().default(3000),
+  HARDCODED_JWT: Joi.string().required(),
 });
